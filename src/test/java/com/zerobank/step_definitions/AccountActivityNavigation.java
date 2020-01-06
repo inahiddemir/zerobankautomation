9f6ff3532f6573ebc80318f16064660d5aade2f5
inahiddemir <i.nahid.demir@gmail.com>
2020-01-05 18:24:34 -0800

    added 2 scenarios

--- src/test/java/com/zerobank/step_definitions/AccountActivityNavigation.java
+++ src/test/java/com/zerobank/step_definitions/AccountActivityNavigation.java
@@ -3,10 +3,13 @@ package com.zerobank.step_definitions;
 import com.zerobank.utilities.ConfigurationReader;
 import com.zerobank.utilities.Driver;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class AccountActivityNavigation {
 
@@ -21,4 +24,27 @@ public class AccountActivityNavigation {
         String expectedTitle = "Zero - Account Summary";
         Assert.assertEquals(expectedTitle, Driver.get().getTitle());
     }
-}
+
+    @When("the user clicks on {string} link on the {string} page")
+    public void the_user_clicks_on_link_on_the_page(String string, String string2) {
+        //string = Savings
+        Assert.assertEquals(string2,Driver.get().getTitle());
+        WebElement element = Driver.get().findElement(By.linkText(string));
+        element.click();
+
+    }
+
+    @Then("the {string} page should be displayed")
+    public void the_page_should_be_displayed(String string) {
+        Assert.assertEquals(string,Driver.get().getTitle());
+    }
+
+    @Then("Account dropdown should have {string} selected")
+    public void account_dropdown_should_have_selected(String string) {
+
+            Select select = new Select(Driver.get().findElement(By.id("aa_accountId")));
+            WebElement DropDownElement = select.getFirstSelectedOption();
+            String DropDownElementText = DropDownElement.getText();
+            Assert.assertEquals(string,DropDownElementText);
+        }
+    }
--- src/test/resources/features/AccountActivityNavigation.feature
+++ src/test/resources/features/AccountActivityNavigation.feature
@@ -1,6 +1,13 @@
 Feature: Navigating to specific accounts in Accounts Activity
+
   Scenario: Savings account redirect
     Given the user is logged in
-    When the user clicks on "Savings" link on the "Account Summary" page
-    Then the "Account Activity" page should be displayed
+    When the user clicks on "Savings" link on the "Zero - Account Summary" page
+    Then the "Zero - Account Activity" page should be displayed
     And Account dropdown should have "Savings" selected
+
+  Scenario: Brokerage account redirect
+    Given the user is logged in
+    When the user clicks on "Brokerage" link on the "Zero - Account Summary" page
+    Then the "Zero - Account Activity" page should be displayed
+    And Account dropdown should have "Brokerage" selected
